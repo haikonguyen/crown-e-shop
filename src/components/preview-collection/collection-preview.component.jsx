48d11e7c8af1e3b031955292e77d58907d4da007
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-10-03 15:32:10 +0200

    rendering collectionItem, and spreading the props

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const collectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -7,11 +8,8 @@ const collectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>
-            {item.name}
-            {/* <img src={item.imageUrl} alt="" /> */}
-          </div>
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem {...itemProps} />
         ))}
     </div>
   </div>
