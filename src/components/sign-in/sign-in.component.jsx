dc7524b0e417c4e6f845ba6f0cd1e70eb56a934b
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-10-04 11:19:50 +0200

    replacing the input with FormInput

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
+import FormInput from '../form-input/form-input.component';
 
 class SignIn extends Component {
   constructor(props) {
@@ -30,22 +31,22 @@ class SignIn extends Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
             required
-            onChange={this.handleChange}
+            label="Email"
+            handleChange={this.handleChange}
           />
-          <label htmlFor="">Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             required
+            label="Password"
           />
-          <label>Password</label>
           <input type="submit" value="Submit" />
         </form>
       </div>
