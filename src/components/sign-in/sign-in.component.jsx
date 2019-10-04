7dc3741badd2ccced766189d5da041fad13c2fa8
Hai Ha Nguyen <haiha.nguyen@starkysclub.com>
2019-10-04 15:17:21 +0200

    getting google features into our comp

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import "./sign-in.styles.scss";
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -49,6 +51,7 @@ class SignIn extends Component {
             label="Password"
           />
           <CustomButton type="submit">SIGN IN</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</CustomButton>
         </form>
       </div>
     );
