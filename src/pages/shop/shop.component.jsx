c8403a39a903770fe038917fdd68af59e2781692
Hai Ha Nguyen <haicorp87@gmai.com>
2020-05-20 22:14:30 +0200

    adding a new collection component, adding routing to shop component, using shop selector to use dynamic routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { Route } from 'react-router-dom';
 
-const ShopPage = ({ collections }) => {
-  console.log('collections', collections);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
