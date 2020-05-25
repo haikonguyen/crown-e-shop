0c997decbaf4bf526d7ad31ed516ab0b7cb78921
Hai Ha Nguyen <haicorp87@gmai.com>
2020-05-26 00:13:23 +0200

    create a new util fn to transform collection with routeName field (missing fron firebase) only used for webapp

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -67,6 +67,24 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const tranformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return tranformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
