45a073c67c386e30c8db8aab78ad6a9447f6d0cd
Hai Ha Nguyen <haicorp87@gmai.com>
2021-03-06 13:06:04 +0100

    adding redux saga to store.js, using composeWithDevTools for Redux Devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-// import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { fetchCollectionsStartSaga } from './shop/shop.sagas';
+
 import rootReducer from './root-reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middlewares))
-);
+const composeEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, composeEnhancers);
+
+sagaMiddleware.run(fetchCollectionsStartSaga);
 
 export const persistor = persistStore(store);
